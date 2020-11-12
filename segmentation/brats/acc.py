bc37aa49f2b3a43037e1638f260e6c228dc9d885
aoji-tjut <55875798+aoji-tjut@users.noreply.github.com>
2020-11-12 10:41:19 +0800

    Add files via upload

--- segmentation/brats/acc.py
+++ segmentation/brats/acc.py
@@ -27,6 +27,8 @@ for label in [1, 2, 3, 4, 5, 6]:
     for channel in range(y_label.shape[2]):
         intersection = 0
         union = 0
+        two = 0 # 分类正确的像素点
+        one = 0 # 所有像素点
         tp = 0
         fn = 0
         fp = 0
@@ -39,6 +41,11 @@ for label in [1, 2, 3, 4, 5, 6]:
                     union += 1
                 if (y_label[row, col, channel] == label and y_pred[row, col, channel] == label):
                     intersection += 1
+                # accuracy
+                if y_label[row, col, channel] == label:
+                    one += 1
+                    if y_pred[row, col, channel] == label:
+                        two += 1
                 # confusion matrix
                 if y_label[row, col, channel] == label and y_pred[row, col, channel] == label:
                     tp += 1
@@ -48,9 +55,10 @@ for label in [1, 2, 3, 4, 5, 6]:
                     fp += 1
                 else:
                     tn += 1
-
         if union != 0 and intersection != 0:
             iou = np.append(iou, intersection / union)
+        if one != 0:
+            accuracy = np.append(accuracy, two / one)
         if (tp + fp) !=0  and tp != 0:
             precision = np.append(precision, (tp) / (tp + fp))
         if (tp + fn) != 0 and tp != 0:
@@ -58,9 +66,11 @@ for label in [1, 2, 3, 4, 5, 6]:
 
     print("label\t\t", label)
     print(iou)
+    print(accuracy)
     print(precision)
     print(recall)
-    print("iou\t\t\t", iou.mean())
+    print("iou\t\t", iou.mean())
+    print("accuracy\t", accuracy.mean())
     print("precision\t", precision.mean())
     print("recall\t\t", recall.mean())
     print("dice\t\t", (2 * precision.mean() * recall.mean()) / (precision.mean() + recall.mean()))
