2b74c23c50b8fbd05f3f53d1c0deca4d952411ce
aoji-tjut <55875798+aoji-tjut@users.noreply.github.com>
2020-11-12 12:25:31 +0800

    Add files via upload

--- segmentation/brats/acc.py
+++ segmentation/brats/acc.py
@@ -7,13 +7,8 @@ import nibabel as nib
 # 4-黄-海马体
 # 5-青-中脑
 # 6-紫-半卵圆中心
-y_label = nib.load("C:/Users/Administrator/Desktop/seg.nii")
-# y_pred = nib.load("C:/Users/Administrator/Desktop/result/pre_64-64-20_32-32-32_16_0.0003_0.5.nii")
-y_pred = nib.load("C:/Users/Administrator/Desktop/Valid_01.nii")
-
-# BraTS2020
-# y_label = nib.load("C:/Users/Administrator/Desktop/result/BraTS2020/seg.nii")
-# y_pred = nib.load("C:/Users/Administrator/Desktop/result/BraTS2020/pre.nii")
+y_label = nib.load("./data/Train/AD/Train_01/Train_01_seg.nii")
+y_pred = nib.load("./ad_validation_predictions/Valid_01.nii")
 
 y_label = np.array(y_label.get_fdata())
 y_pred = np.array(y_pred.get_fdata())
@@ -25,22 +20,15 @@ for label in [1, 2, 3, 4, 5, 6]:
     precision = np.array([])
     recall = np.array([])
     for channel in range(y_label.shape[2]):
-        intersection = 0
-        union = 0
-        two = 0 # 分类正确的像素点
-        one = 0 # 所有像素点
-        tp = 0
-        fn = 0
-        fp = 0
-        tn = 0
+        two = 0             # y_pred正确的像素点
+        one = 0             # y_label所有像素点
+        tp = 0              # y_pred正样本 y_label正样本
+        tn = 0              # y_pred负样本 y_label负样本
+        fp = 0              # y_pred正样本 y_label负样本
+        fn = 0              # y_pred负样本 y_label正样本
 
         for row in range(y_label.shape[0]):
             for col in range(y_label.shape[1]):
-                # IoU
-                if (y_label[row, col, channel] == label or y_pred[row, col, channel] == label):
-                    union += 1
-                if (y_label[row, col, channel] == label and y_pred[row, col, channel] == label):
-                    intersection += 1
                 # accuracy
                 if y_label[row, col, channel] == label:
                     one += 1
@@ -55,9 +43,9 @@ for label in [1, 2, 3, 4, 5, 6]:
                     fp += 1
                 else:
                     tn += 1
-        if union != 0 and intersection != 0:
-            iou = np.append(iou, intersection / union)
-        if one != 0:
+        if (fp + tp + fn) != 0 and tp != 0:
+            iou = np.append(iou, tp / (fp + tp + fn))
+        if one != 0 and two != 0:
             accuracy = np.append(accuracy, two / one)
         if (tp + fp) !=0  and tp != 0:
             precision = np.append(precision, (tp) / (tp + fp))
