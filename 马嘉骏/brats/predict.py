8d54253250dac1a5253c92c2afbf42e2faf00063
aoji-tjut <245611640@qq.com>
2020-12-20 21:20:10 +0800

    12.20

--- 马嘉骏/brats/predict.py
+++ 马嘉骏/brats/predict.py
@@ -61,6 +61,6 @@ def main():
 
 if __name__ == "__main__":
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"
     
     main()
