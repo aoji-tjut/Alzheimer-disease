4ce961a61cf7b11719befda154d9f20ce2e2aff3
aoji-tjut <245611640@qq.com>
2021-02-11 18:00:38 +0800

    2.11

--- segmentation/unet3d/model/dense_net.py
+++ segmentation/unet3d/model/dense_net.py
@@ -13,7 +13,7 @@ def conv_block(x, stage, branch, nb_filter, dropout_rate=None):
     inter_channel = nb_filter * 4
     x = l.BatchNormalization(name=conv_name_base + '_x1_bn', axis=1)(x)
     x = l.Activation('relu', name=relu_name_base + '_x1')(x)
-    x = l.Conv3D(inter_channel, kernel_size=1, strides=1, name=conv_name_base + '_x1', use_bias=False)(x)
+    x = l.Conv3D(inter_channel, kernel_size=1, strides=1, padding="same", name=conv_name_base + '_x1', use_bias=False)(x)
 
     if dropout_rate:
         x = l.Dropout(dropout_rate)(x)
@@ -21,24 +21,11 @@ def conv_block(x, stage, branch, nb_filter, dropout_rate=None):
     # 3x3 Convolution
     x = l.BatchNormalization(name=conv_name_base + '_x2_bn', axis=1)(x)
     x = l.Activation('relu', name=relu_name_base + '_x2')(x)
-    x = l.ZeroPadding3D((1, 1, 1), name=conv_name_base + '_x2_zeropadding')(x)
-    x = l.Conv3D(nb_filter, kernel_size=3, strides=1, name=conv_name_base + '_x2', use_bias=False)(x)
+    #x = l.ZeroPadding3D((1, 1, 1), name=conv_name_base + '_x2_zeropadding')(x)
+    x = l.Conv3D(nb_filter, kernel_size=3, strides=1, padding="same", name=conv_name_base + '_x2', use_bias=False)(x)
 
     if dropout_rate:
         x = l.Dropout(dropout_rate)(x)
-    return x
-
-
-def se_block(x, stage, previous, nb_filter, ratio=16):
-    se_name = 'se' + str(stage) + '_' + previous
-    init = x
-    x = l.GlobalAveragePooling3D(name='global_average_pooling_3D_' + se_name)(x)
-    x = l.Dense(nb_filter // ratio, name='dense_relu_' + se_name)(x)
-    x = l.Activation('relu', name='relu_' + se_name)(x)
-    x = l.Dense(nb_filter, name='dense_sigmoid_' + se_name)(x)
-    x = l.Activation('sigmoid', name='sigmoid_' + se_name)(x)
-    x = tf.expand_dims(x, 1)
-    x = init * tf.expand_dims(x, 1)
     return x
 
 
@@ -63,7 +50,7 @@ def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None):
 
     x = l.BatchNormalization(name=conv_name_base + '_bn', axis=1)(x)
     x = l.Activation('relu', name=relu_name_base)(x)
-    x = l.Conv3D(int(nb_filter * compression), kernel_size=1, strides=1, name=conv_name_base, use_bias=False)(x)
+    x = l.Conv3D(int(nb_filter * compression), kernel_size=1, strides=1, padding="same", name=conv_name_base, use_bias=False)(x)
 
     if dropout_rate:
         x = l.Dropout(dropout_rate)(x)
@@ -76,10 +63,10 @@ def transition_block(x, stage, nb_filter, compression=1.0, dropout_rate=None):
 def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5, dropout_rate=0.3,
                       n_segmentation_levels=3, n_labels=4, optimizer=Adam, initial_learning_rate=5e-4,
                       loss_function=weighted_dice_coefficient_loss, activation_name="sigmoid"):
-    growth_rate = 32
+    growth_rate = 8
     nb_layers = [6, 12, 24, 16]
     reduction = 0
-    batch_size = 32
+#    batch_size = 32
 
     # compute compression factor
     compression = 1.0 - reduction
@@ -91,12 +78,12 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
 
     input = l.Input(shape=input_shape, name='data')
 
-    x = l.ZeroPadding3D((3, 3, 3), name='conv1_zeropadding', batch_size=batch_size)(input)
-    x = l.Conv3D(n_base_filters, kernel_size=7, strides=2, name='conv1', use_bias=False)(x)
+#    x = l.ZeroPadding3D((3, 3, 3), name='conv1_zeropadding', batch_size=batch_size)(input)
+    x = l.Conv3D(n_base_filters, kernel_size=5, strides=1, name='conv1', padding="same", use_bias=False)(input)
     x = l.BatchNormalization(name='conv1_bn', axis=1)(x)
     x = l.Activation('relu', name='relu1')(x)
-    x = l.ZeroPadding3D((1, 1, 1), name='pool1_zeropadding')(x)
-    x = l.MaxPooling3D((3, 3, 3), strides=(2, 2, 2), name='pool1')(x)
+#    x = l.ZeroPadding3D((1, 1, 1), name='pool1_zeropadding')(x)
+#    x = l.MaxPooling3D((3, 3, 3), strides=(2, 2, 2), name='pool1')(x)
 
     stage = 0
     # Add dense blocks
--- segmentation/unet3d/model/u_net.py
+++ segmentation/unet3d/model/u_net.py
@@ -1,19 +1,15 @@
-from functools import partial
+# u-net
 import keras.layers as l
-from keras.layers import Input, LeakyReLU, Add, UpSampling3D, Activation, SpatialDropout3D, Conv3D
 from keras.engine import Model
 from keras.optimizers import Adam
-
-from .unet import create_convolution_block, concatenate
+from ..metrics import dice_coefficient_loss
 from ..metrics import weighted_dice_coefficient_loss
-
-create_convolution_block = partial(create_convolution_block, activation=LeakyReLU, instance_normalization=True)
 
 
 def Conv(input, out_dim):
     conv = l.Conv3D(out_dim, kernel_size=3, strides=1, padding="same")(input)
     bn = l.BatchNormalization()(conv)
-    act = l.ReLU()(bn)
+    act = l.LeakyReLU()(bn)
     conv = l.Conv3D(out_dim, kernel_size=3, strides=1, padding="same")(act)
     bn = l.BatchNormalization()(conv)
     return bn
@@ -33,15 +29,16 @@ def Pool(input):
 
 
 def Concat(x, y):
-    concat = l.Concatenate(axis=-1)([x, y])
+    concat = l.Concatenate(axis=1)([x, y])
     return concat
 
 
 def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5, dropout_rate=0.3,
                       n_segmentation_levels=3, n_labels=4, optimizer=Adam, initial_learning_rate=5e-4,
+                      #loss_function=dice_coefficient_loss, activation_name="sigmoid"):
                       loss_function=weighted_dice_coefficient_loss, activation_name="sigmoid"):
     input = l.Input(input_shape)
-    dim = 16
+    dim = n_base_filters
 
     # Down sampling
     down1 = Conv(input, dim * 1)
@@ -74,12 +71,11 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
     concat5 = Concat(up5, down1)
     conv5 = Conv(concat5, dim * 1)
 
-    output = l.Conv3D(6, kernel_size=3, strides=1, padding="same", activation=l.LeakyReLU)(conv5)
+    conv = l.Conv3D(n_labels, kernel_size=1, strides=1, padding="same")(conv5)
+    output = l.Activation("sigmoid")(conv)
 
     model = Model(inputs=input, outputs=output)
-
     model.summary()
-
-    model = Model(inputs=input, outputs=output)
     model.compile(optimizer=optimizer(lr=initial_learning_rate), loss=loss_function)
+
     return model
